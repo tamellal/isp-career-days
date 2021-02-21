3e75f5e3c7d66f61a0323176f857cc784a23d5e9
Toufik <t.amellal@thecodingmachine.com>
2021-02-21 12:59:59 +0100

    update the map

---
+++ isp-logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="isp-logo" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="../../career-fair/tilesets/isp-logo.png" width="500" height="500"/>
+</tileset>
