761dbc9be470532e6e1617ca4c23a1443490d7cd
tamellal <36163467+tamellal@users.noreply.github.com>
2021-05-02 14:56:34 +0000

    Deploying to gh-pages from master @ bc5c354f5b7b5b8c389e4d726eeb5e3959599f9c 🚀

---
+++ stands.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="stands" tilewidth="32" tileheight="32" tilecount="8649" columns="93">
+ <image source="tilesets/stands.png" width="3000" height="3000"/>
+</tileset>
