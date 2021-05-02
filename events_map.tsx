bf6bc1d04f65ad64cea3767ac0fcb87c7ba2ee02
Toufik <t.amellal@thecodingmachine.com>
2021-05-02 16:46:31 +0200

    update map

---
+++ events_map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="events_map" tilewidth="32" tileheight="32" tilecount="8649" columns="93">
+ <image source="tilesets/events_map.png" width="3000" height="3000"/>
+</tileset>
