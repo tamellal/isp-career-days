aec90f295bb2a8007fef3c5beb4104886778fc59
tamellal <36163467+tamellal@users.noreply.github.com>
2021-05-02 12:41:40 +0000

    Deploying to gh-pages from master @ 3fbdbaf06f31d35f240cd0ee4c42799f0b4c6bac 🚀

--- general/isp-logo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2021.01.13" name="welcome" tilewidth="32" tileheight="32" tilecount="300" columns="20">
- <image source="../tilesets/welcome.png" width="640" height="480"/>
-</tileset>
