3fbdbaf06f31d35f240cd0ee4c42799f0b4c6bac
Toufik <t.amellal@thecodingmachine.com>
2021-05-02 14:40:43 +0200

    Revert "general map"
    This reverts commit e3d1b386b2557b09c1a4a57d2a659272e9799b36.

--- general.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2021.01.13" name="general" tilewidth="32" tileheight="32" tilecount="4200" columns="75">
- <image source="tilesets/general.png" width="2400" height="1800"/>
-</tileset>
